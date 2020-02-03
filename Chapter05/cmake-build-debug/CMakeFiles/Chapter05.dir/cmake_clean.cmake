92013d4af0f239b0eecbd432cce6923579d2185c
Kina <platina.kusakina@gmail.com>
2020-02-03 15:56:56 +0900

    Create Chapter05 Example Code

---
+++ Chapter05/cmake-build-debug/CMakeFiles/Chapter05.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter05.dir/ClassInit.cpp.o"
+  "Chapter05"
+  "Chapter05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
