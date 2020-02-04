90cd234f8b6c4d0206f79760f1b4bc72559d8f78
Kina <platina.kusakina@gmail.com>
2020-02-04 16:41:02 +0900

    Create Chapter10 Example Code

---
+++ Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/CommuMultipleOperation.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/CommuMultipleOperation.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/ConsoleOutput.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/ConsoleOutput.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/FirstOperationOverloading.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/FirstOperationOverloading.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/GFunctionOverloading.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/GFunctionOverloading.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/IterateConsoleOutput.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/IterateConsoleOutput.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/OneOpndOverloading.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/OneOpndOverloading.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/OverloadingOperation.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/OverloadingOperation.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/PointConsoleOutput.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/PointConsoleOutput.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/PointMultipleOperation.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/PointMultipleOperation.cpp.o"
+  "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/PostOpndOverloading.cpp" "/Users/kina/Documents/GitHub/BasicCpp/Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/PostOpndOverloading.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Chapter10/cmake-build-debug/CMakeFiles/Chapter10.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter10.dir/CommuMultipleOperation.cpp.o"
+  "CMakeFiles/Chapter10.dir/ConsoleOutput.cpp.o"
+  "CMakeFiles/Chapter10.dir/FirstOperationOverloading.cpp.o"
+  "CMakeFiles/Chapter10.dir/GFunctionOverloading.cpp.o"
+  "CMakeFiles/Chapter10.dir/IterateConsoleOutput.cpp.o"
+  "CMakeFiles/Chapter10.dir/OneOpndOverloading.cpp.o"
+  "CMakeFiles/Chapter10.dir/OverloadingOperation.cpp.o"
+  "CMakeFiles/Chapter10.dir/PointConsoleOutput.cpp.o"
+  "CMakeFiles/Chapter10.dir/PointMultipleOperation.cpp.o"
+  "CMakeFiles/Chapter10.dir/PostOpndOverloading.cpp.o"
+  "Chapter10"
+  "Chapter10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
