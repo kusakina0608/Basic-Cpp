3ae75d5b7867f4b6870a1302342dfe5577917efa
Kina <platina.kusakina@gmail.com>
2020-02-05 12:28:35 +0900

    Create Chapter12 Example Code

---
+++ Chapter12/cmake-build-debug/CMakeFiles/Chapter12.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter12.dir/STDString.cpp.o"
+  "CMakeFiles/Chapter12.dir/StringClass.cpp.o"
+  "Chapter12"
+  "Chapter12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
